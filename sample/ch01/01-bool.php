bc9ce66048e95aa1be6f031a2d30422b4e561838
konosumi <36559308+konosumi@users.noreply.github.com>
2018-12-29 19:17:43 +0900

    サンプルを追加

---
+++ sample/ch01/01-bool.php
@@ -0,0 +1,13 @@
+<?php
+
+// 論議型の基本形です
+$a = true;
+$b = false;
+echo gettype($a).PHP_EOL; // boolean
+
+// 比較をした結果は真偽値になります
+// var_dumpは変数の内容を出力するコマンドです
+$c = 1;
+$d = 2;
+var_dump($c == $d); // bool(false)
+var_dump($c > $d);  // bool(false)
