6200ff87d8f0c561f26a6b32aec69f0fb09fe56b
konosumi <36559308+konosumi@users.noreply.github.com>
2018-12-29 19:34:02 +0900

    Update 02-int.php

--- sample/ch01/02-int.php
+++ sample/ch01/02-int.php
@@ -1,9 +1,9 @@
 <?php
 
 // 整数型の基本形です
-$a = 3;
-$b = 0;
-$c = -10;
+$a = 3;   // 正の整数
+$b = 0;   // ゼロ
+$c = -10; // 負の整数
 echo gettype($a).PHP_EOL; // integer
 echo gettype($b).PHP_EOL; // integer
 echo gettype($c).PHP_EOL; // integer
