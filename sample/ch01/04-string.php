a26b857adb734c62383a26e55343794ab5e09a29
konosumi <36559308+konosumi@users.noreply.github.com>
2018-12-30 02:05:06 +0900

    Update 04-string.php

--- sample/ch01/04-string.php
+++ sample/ch01/04-string.php
@@ -8,7 +8,7 @@ $a = 'abc';
 $b = 'あいうえお';
 echo gettype($a).PHP_EOL; // string
 
-// 長すぎる文字列ではメモリーが不足する
+// 長すぎる文字列ではメモリーが足りない
 // Fatal error: Allowed memory size of 134217728 bytes
 // exhausted (tried to allocate 2147483679 bytes)
 $c = str_repeat('a', 2147483647);
