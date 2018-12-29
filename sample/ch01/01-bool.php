39d3929b02407b33ab04fe164fd038f67f6cece5
konosumi <36559308+konosumi@users.noreply.github.com>
2018-12-29 19:44:39 +0900

    サンプルの修正

--- sample/ch01/01-bool.php
+++ sample/ch01/01-bool.php
@@ -1,13 +1,17 @@
 <?php
+/**
+ * 論理型
+ */
 
 // 論理型の基本形です
 $a = true;
 $b = false;
+// gettypeは変数の型を返します
 echo gettype($a).PHP_EOL; // boolean
 
-// 比較をした結果は真偽値になります
-// var_dumpは変数の内容を出力するコマンドです
 $c = 1;
 $d = 2;
+// 比較をした結果は真偽値になります
+// var_dumpは変数の内容を出力するコマンドです
 var_dump($c == $d); // bool(false)
 var_dump($c > $d);  // bool(false)
