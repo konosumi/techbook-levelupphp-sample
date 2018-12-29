78cd671ce9fc121612afeeb386296fdef05e3b2c
konosumi <36559308+konosumi@users.noreply.github.com>
2018-12-30 01:07:53 +0900

    整数の修正

--- sample/ch01/01-bool.php
+++ sample/ch01/01-bool.php
@@ -1,17 +1,15 @@
 <?php
 /**
- * 論理型
+ * 論理型(boolean)
  */
 
-// 論理型の基本形です
+// 論理型の基本形
 $a = true;
 $b = false;
-// gettypeは変数の型を返します
 echo gettype($a).PHP_EOL; // boolean
 
 $c = 1;
 $d = 2;
-// 比較をした結果は真偽値になります
-// var_dumpは変数の内容を出力するコマンドです
+// 比較をすると真偽値が返却されます
 var_dump($c == $d); // bool(false)
 var_dump($c > $d);  // bool(false)
--- sample/ch01/02-int.php
+++ sample/ch01/02-int.php
@@ -1,29 +1,27 @@
 <?php
 /**
- * 整数型
+ * 整数型(integer)
  */
 
-// 整数型の基本形です
+// 整数型の基本形
 $a = 3;   // 正の整数
 $b = 0;   // ゼロ
 $c = -10; // 負の整数
 echo gettype($a).PHP_EOL; // integer
-echo gettype($b).PHP_EOL; // integer
-echo gettype($c).PHP_EOL; // integer
 
-// 10進数以外での表現も可能
-$a2  = 0b111;   // 2進数
-$a8  = 0125;    // 8進数 
-$a16 = 0x3A;    // 16進数
+// 10進数以外の表現も可能
+$a2 = 0b111;    // 2進数(先頭に0b)
 var_dump($a2);  // int(7)
-var_dump($a8);  // int(85)
-var_dump($a16); // int(58)
+$a8 = 012;      // 8進数(先頭に0)
+var_dump($a8);  // int(10)
+$a16 = 0x1A;    // 16進数(先頭に0x)
+var_dump($a16); // int(26)
 
-// 整数がとりえる範囲(最大値と最小値)は、定数から取得できる
+// 整数の範囲(最大値と最小値)は、定数から取得できる
 $max = PHP_INT_MAX;
+var_dump($max); // int(9223372036854775807)
 // (PHP7.0)PHP_INT_MIN
 $min = PHP_INT_MIN;
-var_dump($max); // int(9223372036854775807)
 var_dump($min); // int(-9223372036854775808)
 
 // 整数の範囲を超えると桁あふれ(オーバーフロー)現象がおきる
@@ -31,3 +29,8 @@ $overMax = PHP_INT_MAX + 100;
 var_dump($overMax); // float(9.2233720368548E+18)
 $overMin = PHP_INT_MIN - 100;
 var_dump($overMin); // float(-9.2233720368548E+18)
+
+// 桁あふれの環境下では正確な判定ができない
+$over100 = PHP_INT_MAX + 100;
+$over101 = PHP_INT_MAX + 101;
+var_dump($over100 == $over101); // bool(true)
