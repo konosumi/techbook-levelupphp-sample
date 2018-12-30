d2334131924dbef33e10e2d880d1f0bd7b46241e
konosumi <36559308+konosumi@users.noreply.github.com>
2018-12-31 01:08:58 +0900

    Update 04-02-string-memory.php

--- sample/ch01/04-02-string-memory.php
+++ sample/ch01/04-02-string-memory.php
@@ -8,17 +8,16 @@
 // exhausted (tried to allocate 2147483679 bytes)
 //$c = str_repeat('a', 2147483647);
 
-// 文字列が長くなるにつれ、メモリー使用量も増えていく
+// 文字列が長くなるごとにメモリー使用量が増えていく
 $d = '';
 for ($i = 1; $i < 2147483647; $i++) {
     $d .= 'a';
 
     // 100文字ごとに出力する
-    if (strlen($d) % 100 != 0) {
+    if (strlen($d) % 100 !== 0) {
         continue;
     }
 
     // len:130023300 memory:130376552
     echo 'len:'. strlen($d). ' memory:'. memory_get_usage(). PHP_EOL;
 }
-
