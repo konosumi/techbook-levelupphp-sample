fb1b1122b8eee635a4974744a06c9d14bc34a122
konosumi <36559308+konosumi@users.noreply.github.com>
2018-12-30 12:59:57 +0900

    Update 04-02-string-multi.php

--- sample/ch01/04-02-string-multi.php
+++ sample/ch01/04-02-string-multi.php
@@ -11,3 +11,9 @@ echo 'strlen:'. strlen($b). PHP_EOL; // strlen:9
 
 // マルチバイトを加味した文字数の判定
 echo 'mb_strlen:'. mb_strlen($b). PHP_EOL; // mb_strlen:3
+
+// K「半角カタカナ」を「全角カタカナ」に変換します。 
+// a「全角」英数字を「半角」に変換します。 
+// s「全角」スペースを「半角」に変換します。 
+$c = "１　あ　ｲ"; // 全角の1(全角スペース)あ(全角スペース)半角のイ
+echo mb_convert_kana($c, 'Kas'). PHP_EOL; // 1 あ イ
