f547974f503324a5f89c8dd974980d14293bf7f3
konosumi <36559308+konosumi@users.noreply.github.com>
2018-12-31 01:45:49 +0900

    Update 04-03-string-multi.php

--- sample/ch01/04-03-string-multi.php
+++ sample/ch01/04-03-string-multi.php
@@ -3,7 +3,7 @@
  * マルチバイト文字列
  */
 
-// 半角のASCII文字列と、全角の日本語マルチバイト文字列は長さが異なる
+// 半角のASCII文字列と、全角マルチバイト文字列の違い
 $a = 'abc';   // 半角のASCII文字列
 $b = 'あいう'; // 全角の日本語マルチバイト文字列
 echo 'strlen:'. strlen($a). PHP_EOL; // strlen:3
@@ -12,20 +12,20 @@ echo 'strlen:'. strlen($b). PHP_EOL; // strlen:9
 // マルチバイトを加味した文字数の判定
 echo 'mb_strlen:'. mb_strlen($b). PHP_EOL; // mb_strlen:3
 
-/**
- * mb_convert_kanaによる日本語文字列の変換
- * K「半角カタカナ」を「全角カタカナ」に変換します。 
- * a「全角」英数字を「半角」に変換します。 
- * s「全角」スペースを「半角」に変換します。 
- */
+// mb_convert_kana()による日本語文字列の変換
 // (変換前)全角の1(全角スペース)あ(全角スペース)半角のイ
 $c = "１　あ　ｲ";
 // (変換後)半角の1(半角スペース)あ(半角スペース)全角のイ
 $d = mb_convert_kana($c, 'Kas');
 echo $c. PHP_EOL; // １　あ　ｲ
 echo $d. PHP_EOL; // 1 あ イ
+/**
+ * K「半角カタカナ」を「全角カタカナ」に変換します。
+ * a「全角」英数字を「半角」に変換します。
+ * s「全角」スペースを「半角」に変換します。
+ */
 
-// mb_detect_encodingによる文字コードの検出
+// mb_detect_encoding()による文字コードの検出
 $e = 'あいう';
 echo mb_detect_encoding($e, 'auto'). PHP_EOL; // UTF-8
 
