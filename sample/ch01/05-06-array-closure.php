4c571fe00cbce9284437a575e2fe7eb1917b22c9
konosumi <36559308+konosumi@users.noreply.github.com>
2018-12-30 18:36:13 +0900

    Create 05-06-array-closure.php

---
+++ sample/ch01/05-06-array-closure.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 配列関数における無名関数の活用
+ */
+
+$array = [1, 2, 3];
+$func = function($v) { return $v * 2; };
+
+// 即席で作った無名関数を使い、配列の各要素を倍にする
+// [0]=> int(2) [1]=> int(4) [2]=> int(6)
+var_dump(array_map($func, $array));
+// または直接書くこともできる
+var_dump(array_map(function($v) { return $v * 2; }, $array));
