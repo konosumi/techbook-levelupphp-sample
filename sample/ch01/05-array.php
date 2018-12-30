8e7306d1a94f5744dd8808866e703c0a97beb729
konosumi <36559308+konosumi@users.noreply.github.com>
2018-12-30 17:02:21 +0900

    Update 05-array.php

--- sample/ch01/05-array.php
+++ sample/ch01/05-array.php
@@ -3,15 +3,15 @@
  * 配列
  */
 
-// 配列の基本形
-// (PHP5.4)配列の短縮表記([])
-// 数値配列
+// 数値配列の基本形
 $a = [1, 2];
-var_dump($a); // array(2) { [0]=> int(1) [1]=> int(2) }
+// array(2) {[0]=> int(1) [1]=> int(2)}
+var_dump($a);
 
-// 連想配列
+// 連想配列の基本形
 $b = [
     "a" => "b",
     "c" => "d",
 ];
-var_dump($b); // array(2) { ["a"]=> string(1) "b" ["c"]=> string(1) "d" }
+// array(2) {["a"]=> string(1) "b" ["c"]=> string(1) "d"}
+var_dump($b);
