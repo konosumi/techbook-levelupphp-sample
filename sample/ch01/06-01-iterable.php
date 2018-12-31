507c99b0c8c4d4f16dcf937366260dcb98dfc015
konosumi <36559308+konosumi@users.noreply.github.com>
2018-12-31 11:47:26 +0900

    Update 06-01-iterable.php

--- sample/ch01/06-01-iterable.php
+++ sample/ch01/06-01-iterable.php
@@ -9,3 +9,28 @@ var_dump(is_iterable(new ArrayIterator([1, 2, 3])));  // bool(true)
 var_dump(is_iterable((function () { yield 1; })()));  // bool(true)
 var_dump(is_iterable(1));  // bool(false)
 var_dump(is_iterable(new stdClass()));  // bool(false)
+
+// ArrayIteratorを使ってみる
+$a = new ArrayIterator([1, 2]);
+foreach ($a as $val) {
+    // ArrayIterator:1
+    // ArrayIterator:2
+    echo 'ArrayIterator:'.$val.PHP_EOL;
+}
+
+// ジェネレーターでyieldを使ってみる
+function yieldtest() {
+    for ($i = 1; $i < 3; $i++) {
+        // 1回yieldする度に、関数呼び出し側のループが1つ進む
+        yield $i;
+        echo 'yield sareta!'.PHP_EOL;
+    }
+}
+
+foreach (yieldtest() as $val) {
+    // yield:1
+    // yield sareta!
+    // yield:2
+    // yield sareta!
+    echo 'yield:'.$val.PHP_EOL;
+}
