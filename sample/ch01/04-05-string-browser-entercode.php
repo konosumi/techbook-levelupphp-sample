63ec711ed93c0d470d4ebfd18d77a4ddcd8b9f23
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-01 10:47:28 +0900

    Create 04-05-string-browser-entercode.php

---
+++ sample/ch01/04-05-string-browser-entercode.php
@@ -0,0 +1,20 @@
+<?php                                                                       
+/**
+ * ブラウザと改行コード
+ * (筆者は、PHPのビルトインウェブサーバーで実験しました)
+ * 
+ * cd sample/ch01/
+ * php -S localhost:8000
+ * http://localhost:8000/04-05-string-browser-entercode.php
+ */
+
+// 送信されたテキストエリアにCR+LFがあるかどうか
+echo strpos($_POST['test'],  "\r\n").'<br>'.PHP_EOL;
+?>
+
+<!-- 送信用フォーム -->
+<form method="post">
+  <textarea name="test">あいうえお
+かきくけこ</textarea>
+  <input type="submit" value="送信する">
+</form>
