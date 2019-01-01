b1bbe4670f4940146c3bf688f65bb4f26cbc4b26
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-01 12:50:28 +0900

    Create 07-01-object.php

---
+++ sample/ch01/07-01-object.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * オブジェクト(object)
+ */
+
+// 1. PHPが言語レベルで用意しているクラスのインスタンス
+$instance1 = new Datetime();
+// 2. ユーザー自身が定義したクラスのインスタンス
+class MyTest {}
+$instance2 = new MyTest();
+// 3. 標準クラスのインスタンス
+$instance3 = new stdClass();
+
+// is_object()を使った、オブジェクトかどうかの判定
+var_dump(is_object($instance1)); // bool(true)
+var_dump(is_object($instance2)); // bool(true)
+var_dump(is_object($instance3)); // bool(true)
