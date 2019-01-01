b584f8bc5ab8e0cacef99ea321dfe87d64d7613c
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-01 13:53:34 +0900

    Update 07-01-object.php

--- sample/ch01/07-01-object.php
+++ sample/ch01/07-01-object.php
@@ -10,8 +10,11 @@ class MyTest {}
 $instance2 = new MyTest();
 // 3. 標準クラスのインスタンス
 $instance3 = new stdClass();
+// 4. 無名クラスのインスタンス
+$instance4 = new class {};
 
 // is_object()を使った、オブジェクトかどうかの判定
 var_dump(is_object($instance1)); // bool(true)
 var_dump(is_object($instance2)); // bool(true)
 var_dump(is_object($instance3)); // bool(true)
+var_dump(is_object($instance4)); // bool(true)
