5323c0bdc8e54361f8dbdea17a5b3e67106eeb87
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-01 16:31:00 +0900

    Update 07-01-object.php

--- sample/ch01/07-01-object.php
+++ sample/ch01/07-01-object.php
@@ -18,3 +18,26 @@ var_dump(is_object($instance1)); // bool(true)
 var_dump(is_object($instance2)); // bool(true)
 var_dump(is_object($instance3)); // bool(true)
 var_dump(is_object($instance4)); // bool(true)
+
+// JSONをパースすると標準クラスになる
+$json = '{
+    "title": "example",
+    "price": 1000,
+    "flags": { "onsale": true, "discount": false }
+}';
+$object = json_decode($json);
+
+var_dump($object->title); // string(7) "example"
+var_dump($object->flags->onsale); // bool(true)
+// 中身は以下のような標準クラスのインスタンス(オブジェクト)です
+var_dump($object);
+/**
+ * object(stdClass)#5 (3) {
+ *   ["title"]=> string(7) "example"
+ *   ["price"]=> int(1000)
+ *   ["flags"]=> object(stdClass)#6 (2) {
+ *     ["onsale"]=> bool(true)
+ *     ["discount"]=> bool(false)
+ *   }
+ * }
+ */
