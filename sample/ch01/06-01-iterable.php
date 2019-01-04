706b7e0b0af7644651085877a1b3653da0f92c08
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-05 02:12:33 +0900

    Update 06-01-iterable.php

--- sample/ch01/06-01-iterable.php
+++ sample/ch01/06-01-iterable.php
@@ -3,14 +3,14 @@
  * 繰り返し可能(iterable)
  */
 
-// is_iterable()を使った、繰り返し可能かどうかの判定
+/** is_iterable()を使った、繰り返し可能かどうかの判定 **/
 var_dump(is_iterable([1, 2, 3]));  // bool(true)
 var_dump(is_iterable(new ArrayIterator([1, 2, 3])));  // bool(true)
 var_dump(is_iterable((function () { yield 1; })()));  // bool(true)
 var_dump(is_iterable(1));  // bool(false)
 var_dump(is_iterable(new stdClass()));  // bool(false)
 
-// イテレータ
+/** イテレータ **/
 // implementsで、インターフェースの実装を宣言しています
 class MyIterator implements Iterator {
     private $position = 0;
