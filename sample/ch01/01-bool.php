123ffc760ec5e7f2f1d23f725a75378faa02113e
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-05 01:24:21 +0900

    サンプルのコメントを変更

--- sample/ch01/01-bool.php
+++ sample/ch01/01-bool.php
@@ -3,12 +3,12 @@
  * 論理型(boolean)
  */
 
-// 論理型の基本形
+/** 論理型の基本形 **/
 $a = true;
 $b = false;
 echo gettype($a).PHP_EOL; // boolean
 
-// 比較をすると真偽値が返却されます
+/** 比較をすると真偽値が返却されます **/
 $c = 1;
 $d = 2;
 var_dump($c === $d); // bool(false)
