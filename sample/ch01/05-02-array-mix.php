3d2160e542a3a45c93d914852754ace8509f743f
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-05 02:02:38 +0900

    サンプルの修正

--- sample/ch01/05-02-array-mix.php
+++ sample/ch01/05-02-array-mix.php
@@ -3,7 +3,7 @@
  * 配列の混在によるトラブル
  */
 
-// 数値配列と連想配列の混在によるトラブル
+/** 数値配列と連想配列の混在によるトラブル **/
 $mixArray = [1, 2, 'fruits' => 'apple', 4];
 // [0]=> 1 [1]=> 2 ["fruits"]=> "apple" [2]=> 4
 var_dump($mixArray);
@@ -16,7 +16,7 @@ for ($i = 0; $i < $count; $i++) {
     var_dump($mixArray[$i]);
 }
 
-// foreachを使えば、数値配列と連想配列を両方とも取得できる
+/** foreachを使えば、数値配列と連想配列を両方とも取得できる **/
 foreach ($mixArray as $key => $val) {
     // 0 => 1, 1 => 2, fruits => apple, 2 => 4
     echo $key. ' => '. $val.PHP_EOL;
