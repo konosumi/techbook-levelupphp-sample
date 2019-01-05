1429a8f67151731b19e5f2f04128c925bd4b5ae1
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-05 14:56:53 +0900

    Update 01-typeconvert.php

--- sample/03typeconvert/01-typeconvert.php
+++ sample/03typeconvert/01-typeconvert.php
@@ -51,3 +51,26 @@ var_dump(in_array($c, ['0A', '1B', '2C']));
 // bool(false)
 var_dump(in_array($c, ['0A', '1B', '2C'], true));
 //}
+
+/**
+ * 論理型(bool)への変換が動くifの変数判定
+ */
+// 以下の例はすべてfalseに変換されるので、何も出力されません
+
+// booleanのFALSE
+if (false) { echo "TRUE!"; }
+// intの0とfloatの0.0
+if (0 || 0.0) { echo "TRUE!"; }
+// 空の文字列と文字列の "0"
+if ("" || "0") { echo "TRUE!"; }
+// 要素数がゼロの配列
+if ([]) { echo "TRUE!"; }
+// 特別な値 NULL (値がセットされていない変数を含む)
+if (NULL) { echo "TRUE!"; }
+
+// 空のタグから作成された SimpleXML オブジェクト
+$xml_string = <<<EOF
+<?xml version="1.0" encoding="utf-8" ?>
+<image />
+EOF;
+if (new SimpleXMLElement($xml_string)) { echo "TRUE!"; }
