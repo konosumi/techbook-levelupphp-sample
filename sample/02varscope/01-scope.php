2ee8c71455d7cd2df55ee2821ef7e9a801bc30bc
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-05 13:42:59 +0900

    Update 01-scope.php

--- sample/02varscope/01-scope.php
+++ sample/02varscope/01-scope.php
@@ -21,3 +21,8 @@ echo PHP_EOL;
 /** グローバルスコープ **/
 // クラスや関数の外で定義している普通の変数
 $str = 'abcde';
+
+/** 定義済みの円周率定数を再定義してみる */
+error_reporting(E_ALL);
+// Notice: Constant M_PI already defined 
+define('M_PI', 3.14);
