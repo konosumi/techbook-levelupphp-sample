5152c731cc2cba767edb38ef151e81e6d551a6b5
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-06 02:52:50 +0900

    Create 05-11-array-variable.php

---
+++ sample/01vartype/05-11-array-variable.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * (PHP5.6)...による可変長引数(配列)
+ */
+
+ // $numbersは引数の数によって増減する配列となる
+function sum(...$numbers) {
+    $acc = 0;
+    foreach ($numbers as $n) {
+        $acc += $n;
+    }
+    return $acc;
+}
+
+var_dump(sum(1, 2, 3, 4));
