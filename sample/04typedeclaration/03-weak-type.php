2f949d03824e85e25dd7a94343ea10e50b0b8de9
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-06 01:21:26 +0900

    Create 03-weak-type.php

---
+++ sample/04typedeclaration/03-weak-type.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 弱い型付け
+ */
+final class SampleWeak
+{
+    // 整数に"変換できるモノ"だけ扱います。
+    public static function echoUserIdWeak(int $userId)
+    {
+        echo "userId=".$userId.PHP_EOL;
+    }
+}
+
+// 先頭が数値である文字列は、暗黙的型変換で数値に変換できる
+// userId=123
+SampleWeak::echoUserIdWeak('123abcde');
