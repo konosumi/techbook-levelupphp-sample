6834384a2416e69882e8862083b36365f349844c
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-06 09:15:01 +0900

    Update 04-function-const.php

--- sample/ch05/04-function-const.php
+++ sample/ch05/04-function-const.php
@@ -4,13 +4,13 @@
  */
 namespace sample\ch05;
 
-// 名前空間の元で関数を定義する
+// 名前空間の下で関数を定義する
 function sampleFunction()
 {
     echo 'Sample function!'.PHP_EOL;
 }
 
-// 名前空間の元で定数を定義する
+// 名前空間の下で定数を定義する
 // constはクラスの中で定義するのが普通なので、推奨する書き方ではありません
 const SAMPLE_DEFINE = 'Sample function!'.PHP_EOL;
 
