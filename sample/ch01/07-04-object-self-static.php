a96fe4adae015d8f18aa6fe27364ccd0306f6017
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-07 01:37:49 +0900

    Update 07-04-object-self-static.php

--- sample/ch01/07-04-object-self-static.php
+++ sample/ch01/07-04-object-self-static.php
@@ -7,13 +7,13 @@ class A {
         // 定義した時点で、__CLASS__=自分自身(Aクラス)となる
         echo __CLASS__.PHP_EOL;
     }
-    public static function test1() {
+    public static function testSelf() {
         // 定義した時点で、self=自分自身(Aクラス)となる
         self::who();
     }
-    public static function test2() {
-        // staticが誰を指すのかは、実行時の段階で決定される
-        // 他のクラスがAを継承した場合、static=継承先のクラスとなる
+    public static function testStatic() {
+        // staticが誰を指し示すのかは、実行時の段階で決定される
+        // Aを継承した他のクラスから呼び出された場合、static=継承先のクラスとなる
         static::who();
     }
 }
@@ -24,5 +24,5 @@ class B extends A {
     }
 }
 
-B::test1(); // Aクラスのwho()が呼び出される
-B::test2(); // Bクラスのwho()が呼び出される
+B::testSelf(); // Aクラスのwho()が呼び出される(self)
+B::testStatic(); // Bクラスのwho()が呼び出される(static)
