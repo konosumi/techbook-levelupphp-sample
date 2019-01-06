78bfbeabe1471e862b746ed3e1b408283dc5587a
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-07 00:43:03 +0900

    Update 07-03-object-trait.php

--- sample/ch01/07-03-object-trait.php
+++ sample/ch01/07-03-object-trait.php
@@ -2,7 +2,14 @@
 /**
  * (PHP5.4)Traitによるクラスの拡張
  */
+
+// traitの定義
 trait transport {
+    // 静的関数
+    static function test() {
+        echo "test!".PHP_EOL;
+    }
+    // インスタンス関数
     function canRide() {
         echo "Can Ride!".PHP_EOL;
     }
@@ -16,17 +23,16 @@ class Base {
 
 // TaxiクラスはBaseを継承している
 class Taxi extends Base {
-    // PHPは多重継承ができないが、traitを使って拡張はできる
+    // PHPではクラスの多重継承はできないが、traitを使って拡張はできる
     use transport;
 }
-
 class Train extends Base {
     // traitは他のクラスでも使い回すことができる
     use transport;
 }
 
-// useで実装したtraitの関数は、あたかもインスタンスが持つクラスと同じように使える
+// useで実装したtraitの関数は、
+// あたかもオブジェクト自身が持つ関数と同じように使える
+Taxi::test(); // test!
 $taxi = new Taxi();
 $taxi->canRide();
-$train = new Train();
-$train->canRide();
