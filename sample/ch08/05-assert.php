f4fee46d6a3d8b80363824a8727abd8d60007cd6
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-10 01:04:52 +0900

    Create 05-assert.php

---
+++ sample/ch08/05-assert.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * (PHP7.0)assertによる簡易テスト
+ */
+
+// assertに失敗したら例外をスローする
+ini_set('assert.exception', 1);
+
+// 配列内に想定する添字があることを担保する
+// PHPの配列の型宣言が、あまり役に立たない弱点を補う
+function assertArray(array $array) {
+    assert(array_key_exists('c', $array));
+    echo 'Assert pass!'.PHP_EOL;
+}
+
+// Assert pass!
+assertArray([
+    "c" => 3,
+]);
+
+// Fatal error: Uncaught AssertionError:
+// assert(array_key_exists('c', $array)) 
+assertArray([
+    "a" => 1,
+    "b" => 2,
+]);
