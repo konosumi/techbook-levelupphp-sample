6de0d265dac6f4a900551743ba1244582f8002e4
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-10 23:30:11 +0900

    パフォーマンス計測

---
+++ sample/ch13/01-performance.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * パフォーマンス計測のシンプルな基本形
+ */
+function test($count) {
+    $sum = 0;
+    for($i = 0; $i < $count; $i ++) {
+        $sum++;
+    }
+}
+
+$start = microtime(true);
+test(10000000);
+$end  = microtime(true);
+
+// est()に0.18110203742981秒かかりました
+echo 'test()に'.($end -  $start).'秒かかりました'.PHP_EOL;
