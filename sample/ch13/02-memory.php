d75b29728897ada03fa911133b9860801159f4f5
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-11 01:36:46 +0900

    Update 02-memory.php

--- sample/ch13/02-memory.php
+++ sample/ch13/02-memory.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * メモリー使用量の計測
+ * メモリ使用量の計測
  */
 function test($count) {
     $str = '';
@@ -9,14 +9,15 @@ function test($count) {
     }
 }
 
+// memory_get_usage()：PHPがスクリプトに割り当てたメモリ量
+// memory_get_usage(true)：システムが実際に割り当てたメモリ量
 $start = memory_get_usage();
 $startActual = memory_get_usage(true);
 test(10000000);
 $end = memory_get_usage();
 $endActual = memory_get_usage(true);
 
-// PHPがスクリプトに割り当てているメモリー使用量は0バイト増えました
-echo 'PHPがスクリプトに割り当てているメモリー使用量は'.($end - $start).'バイト増えました'.PHP_EOL;
-
-// システムが実際に割当てたメモリーは2097152バイト増えました
-echo 'システムが実際に割当てたメモリーは'.($endActual - $startActual).'バイト増えました'.PHP_EOL;
+// PHPがスクリプトに割り当てたメモリ量は0バイト増えました
+echo 'PHPがスクリプトに割り当てたメモリ量は'.($end - $start).'バイト増えました'.PHP_EOL;
+// システムが実際に割り当てたメモリ量は2097152バイト増えました
+echo 'システムが実際に割り当てたメモリ量は'.($endActual - $startActual).'バイト増えました'.PHP_EOL;
