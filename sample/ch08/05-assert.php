094c4689785d6b832cbb7482fdc2c88b10d70b11
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-14 01:54:32 +0900

    Update 05-assert.php

--- sample/ch08/05-assert.php
+++ sample/ch08/05-assert.php
@@ -6,7 +6,7 @@
 // assertに失敗したら例外をスローする
 ini_set('assert.exception', 1);
 
-// 配列内に前提とする要素名ががあることを表明する
+// 配列内に前提とする要素名があることを表明する
 // PHPの配列の型宣言が、あまり役に立たない弱点を補う
 function assertArray(array $array) {
     assert(array_key_exists('c', $array));
