e5b34cf629583276f2e6f0d065e68b36188ea6dc
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-14 13:57:23 +0900

    Update 03-finally.php

--- sample/ch08/03-finally.php
+++ sample/ch08/03-finally.php
@@ -1,14 +1,31 @@
 <?php
 /**
- * ユーザー例外(\Exception)
+ * (PHP5.5)try/catchのfinallyブロック
  */
-final class SampleException extends \Exception {}
+class RandomException extends \Exception {}
 
-try {
-    if (1 !== 0) {
-        throw new SampleException('1 is not 0.');
+function writeFile($file) {
+    $fp = null;
+    try {
+        $fp = fopen($file, 'w');
+        if (rand(0, 1) > 0.5) {
+            throw new \RandomException('ランダムに失敗しました');
+        } else {
+            throw new \Exception('処理が失敗しました');
+        }
+    } catch (RandomException $e) {
+        echo $e->getMessage().PHP_EOL; // ランダムに失敗しました
+    } catch (Exception $e) {
+        echo $e->getMessage().PHP_EOL; // 処理が失敗しました
+    } finally {
+        // try/catch内でどの経路を辿ったとしても、必ず最後に実行したい処理を記述
+        echo 'ファイルポインタは開かれていても必ず閉じます'.PHP_EOL;
+        if (is_resource($fp)) {
+            fclose($fp);
+        }
     }
-} catch (\SampleException $e) {
-    // 1 is not 0.
-    echo $e->getMessage().PHP_EOL;
 }
+
+// ランダムに失敗しました
+// ファイルポインタは開かれていても必ず閉じます
+writeFile('test.txt');
