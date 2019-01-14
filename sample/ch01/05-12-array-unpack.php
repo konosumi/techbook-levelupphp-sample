6e31289c39838ee8337dc358e71998348f8b26dc
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-14 13:17:22 +0900

    Update 05-12-array-unpack.php

--- sample/ch01/05-12-array-unpack.php
+++ sample/ch01/05-12-array-unpack.php
@@ -12,3 +12,20 @@ function fruits($a, $b, $c) {
 // 配列を展開して引数に渡します
 $array = ['りんご', 'みかん', 'バナナ'];
 fruits(...$array);
+
+/** 引数展開で過不足があるとどうなるのか */
+
+// 配列のほうが長いと先頭の2つだけ使われる
+function fruits2($a, $b) {
+    echo $a.PHP_EOL; // りんご
+    echo $b.PHP_EOL; // みかん
+}
+$array = ['りんご', 'みかん', 'バナナ'];
+fruits2(...$array);
+
+// 配列が短いと失敗する
+function fruits3($a, $b, $c, $d) { }
+$array = ['りんご', 'みかん', 'バナナ'];
+fruits3(...$array);
+// Fatal error: Uncaught ArgumentCountError:
+// Too few arguments to function fruits3(),
