0c06278d2828762a980fa2113fb6a5de45f7f019
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-14 23:05:26 +0900

    Update 02-short-open-tag.php

--- sample/ch12/02-short-open-tag.php
+++ sample/ch12/02-short-open-tag.php
@@ -4,7 +4,16 @@
  */
 $val = 'aiueo';
 ?>
+<?= $val.PHP_EOL // aiueo ?>
 
-あいうえお
-<?= $val /* echo $valと同義です */ ?>
-
+<?php
+/**
+ * 以下のように、PHP起動時にphp.iniディレクティブを切り替えて動作確認ができます
+ * php -d short_open_tag=false /path/to/sample.php
+ * 
+ * short_open_tagの設定によって、<?が動くかどうかが決まります
+ * short_open_tag=false <? echo $val.PHP_EOL ?>
+ * short_open_tag=true  aiueo
+ */
+?>
+<? echo $val.PHP_EOL ?>
