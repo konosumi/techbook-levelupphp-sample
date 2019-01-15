e43953fa200c81eca78f34cc2a899215557cce4b
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-16 01:34:43 +0900

    Update 02-preg-quote.php

--- sample/ch11/02-preg-quote.php
+++ sample/ch11/02-preg-quote.php
@@ -4,17 +4,17 @@
  */
 
 // 置換対象の文字列
-$str = 'aa*b*cc';
+$str = 'a*b*c';
 // "a*b*c"という文字列を"d"にする構想です
 [$before, $after] = ["a*b*c","d"];
 
-// 結果がaa*b*ddになってしまう例
+// 結果がa*b*dになってしまう例
 // 理由は、アスタリスク(*)が正規表現におけるメタ文字だからです
 $result = preg_replace('/'.$before.'/', $after, $str);
 echo $result.PHP_EOL;
 
 // メタ文字のアスタリスク(*)ではなく、単純な文字として判定させます
-// 結果：adc
-$pregBefore = preg_quote($before, '/');
-$result = preg_replace('/'.$pregBefore.'/', $after, $str);
+// 結果：d
+$quoted = preg_quote($before, '/');
+$result = preg_replace('/'.$quoted.'/', $after, $str);
 echo $result.PHP_EOL;
