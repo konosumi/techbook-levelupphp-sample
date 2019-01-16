a417c8943a278efe32fa8be591a9894b2bd83c14
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-17 00:24:10 +0900

    Update 07-mb-ereg.php

--- sample/ch11/07-mb-ereg.php
+++ sample/ch11/07-mb-ereg.php
@@ -5,5 +5,6 @@
 
 // 文字列の先頭からマッチさせて一致する文字があるか調べる
 var_dump(mb_ereg_match('.*あい', '雛鶴あい')); // bool(true)
-var_dump(mb_ereg('あい', '雛鶴あい')); // int(1)
+
+// 文字列の置換
 var_dump(mb_ereg_replace('あい', 'aa', '雛鶴あい')); // string(8) "雛鶴aa"
