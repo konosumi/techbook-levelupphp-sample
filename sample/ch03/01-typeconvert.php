66884eae30a68180388629f08d76f67434a42199
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-19 18:14:36 +0900

    Update 01-typeconvert.php

--- sample/ch03/01-typeconvert.php
+++ sample/ch03/01-typeconvert.php
@@ -110,7 +110,7 @@ $foo = "10.0 pigs " + 1.0;      // $foo は float です (11)
 var_dump((string) true);  // string(1) "1"
 var_dump((string) false); // string(0) ""
 
-/** その他の型から文字列への変換 */
+/** 配列やオブジェクトから文字列への変換 */
 var_dump((string) ["abc", "def"]); // string(5) "Array"
 // Recoverable fatal error: Object of class stdClass
 // could not be converted to string 
