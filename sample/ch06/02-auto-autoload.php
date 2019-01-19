15b88d2936866952f0dbe669658dd08a220c4661
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-20 00:36:39 +0900

    Update 02-auto-autoload.php

--- sample/ch06/02-auto-autoload.php
+++ sample/ch06/02-auto-autoload.php
@@ -10,13 +10,12 @@ spl_autoload_register(function ($classname) {
     // sample\ch06\WelcomeAutoloadを分解してパスを組み立てます
     $loadClassFile = realpath($pjRootPath.$ds
         .implode($ds, explode('\\', $classname)).'.php');
-
-    // 新しく読み込みます: [省略]/sample/ch06/WelcomeAutoload.php
-    echo '新しく読み込みます: '.$loadClassFile.PHP_EOL;
-
     if (!$loadClassFile) {
         return;
     }
+
+    // 新しく読み込みます: [省略]/sample/ch06/WelcomeAutoload.php
+    echo '新しく読み込みます: '.$loadClassFile.PHP_EOL;
 
     // プロジェクト直下から辿ると、全てのPHPファイルが対象になってしまう
     // 今回の自動読み込みはサンプル下だけに限定しておきます
