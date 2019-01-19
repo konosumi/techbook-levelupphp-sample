daa48372ebf575b5679867fe4d397873b3210d07
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-20 00:51:19 +0900

    composerの場所を変更

--- sample/ch07/01-naoki-hanzawa.php
+++ sample/ch07/01-naoki-hanzawa.php
@@ -7,8 +7,7 @@
 
 // 実用レベルでは、基盤・上流側の処理で予めrequireしておきます
 // 今回はサンプルなので、暫定的にディレクトリを上がって読み込ます
-require implode(DIRECTORY_SEPARATOR,
-    [__DIR__, '..', '..', 'vendor', 'autoload.php']);
+require 'vendor/autoload.php';
 
 // 2 * 倍返しだ！ = 4 
 echo "2 * 倍返しだ！ = ".NaokiHanzawa::baigaeshida(2).PHP_EOL;
