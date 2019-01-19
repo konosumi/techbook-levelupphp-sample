9418eba57da3c3199330fae6fe776a0d8ff90b7e
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-20 00:24:16 +0900

    Update 01-autoload.php

--- sample/ch06/01-autoload.php
+++ sample/ch06/01-autoload.php
@@ -9,26 +9,26 @@ spl_autoload_register(function ($classname) {
     echo '新しく読み込みます: '.$classname.PHP_EOL;
     $ds = DIRECTORY_SEPARATOR;
 
-    // クラスの名前空間を含めた住所と、実際のファイルをマッピングします
+    // クラスの名前空間を含めたパスと、実際のファイルパスをマッピングします
     $autoloadConfig = [
         'sample\ch06\WelcomeAutoload' =>
             __DIR__.$ds.'WelcomeAutoload.php'
     ];
 
     // クラスが定義されているPHPファイルを読み込みます
-    // これで使えるようにします
+    // include()により、クラスが使えるようになります
     if (isset($autoloadConfig[$classname])) {
         include($autoloadConfig[$classname]);
     }
 });
 
-/**
- * 初回の呼び出しだけ、クラスの読み込みが走っていることが分かります
- * 新しく読み込みます: sample\ch06\WelcomeAutoload
- * Welcome to autoload!
- * Welcome to autoload!
- */ 
+// 初回の呼び出しだけ、クラスの読み込みが走っていることが分かります
+
+// 新しく読み込みます: sample\ch06\WelcomeAutoload
+// Welcome to autoload!
 $sample = new \sample\ch06\WelcomeAutoload();
 $sample->welcome();
+
+// Welcome to autoload!
 $sample2 = new \sample\ch06\WelcomeAutoload();
 $sample2->welcome();
