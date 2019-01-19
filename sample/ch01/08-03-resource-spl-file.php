18985e3e63b89df0f36c81462ca6f2133dd78e1c
konosumi <36559308+konosumi@users.noreply.github.com>
2019-01-19 17:09:41 +0900

    Update 08-03-resource-spl-file.php

--- sample/ch01/08-03-resource-spl-file.php
+++ sample/ch01/08-03-resource-spl-file.php
@@ -13,7 +13,10 @@ unset($spl); // 変数が消えると自動的にcloseされる
 // 読み込み
 $spl = new SplFileObject($file, 'r');
 foreach ($spl as $line) {
-    echo $line; // a(改行)b(改行)c(改行)と出力される
+    echo $line;
+    // a(改行)
+    // b(改行)
+    // c(改行)
 }
 unset($spl); // 変数が消えると自動的にcloseされる
 
